5c367def980ad00a3afc23ee457b2242f1d438fc
nelson-nunez <30480615+nelson-nunez@users.noreply.github.com>
2017-07-30 12:43:38 -0300

    calculo de longitud ahora como funcion :v

--- ejemplo python.py
+++ ejemplo python.py
@@ -1,11 +1,14 @@
 
+
+
+def largo(palabra):
+	print(len(palabra))
+	 
+#------------------------
 palabra = input()
-
-a = len(palabra)
-
 print ("La palabra ingresada es: ", palabra)
-print ("Longitud: ", a)
-print (a)
+print("La longitud de la palabra es: ")
+largo(palabra)
 
 input(" ")
 
