92e7ce0e93c65ce2cea41539a9435acf393bb359
BuildTools <unconfigured@null.spigotmc.org>
2017-08-05 11:49:09 -0300

    funcion que pide al usuario una letra.
    Pide una letra, y permite  al usuario ingresarla. NO permite ingresar mas de una o ninguna.

---
+++ PedirLetra.py
@@ -0,0 +1,15 @@
+
+
+#
+#Imprime mensaje pidiendo al usuario una letra.
+#Luego devuelve la letra q el usuario ingreso.
+#
+#no deja: no poner ninguna letra o mas una.
+
+def PedirLetra():
+
+	a = "aa"
+	while len(a) != 1:
+		a = input("Ingrese Una Letra: ")
+		pass
+	return a
