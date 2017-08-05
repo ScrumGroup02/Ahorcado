871529bd6aae71bd554832da073fbca4b249b6a1
BuildTools <unconfigured@null.spigotmc.org>
2017-08-05 11:50:19 -0300

    Principal, provisorio.
    solo pruebas.

---
+++ Principal.py
@@ -0,0 +1,19 @@
+
+import DarIncongnita
+import PedirLetra
+
+incognita = DarIncongnita.DarIncognita()
+
+print(incognita)
+
+
+print("")
+print("")
+letra = PedirLetra.PedirLetra()
+
+print("")
+print("")
+print(letra)
+
+print("")
+print("")
