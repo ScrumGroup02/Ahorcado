1641c78df904c258019f68ea0919abce342c30cd
BrhmDnsK263 <Denysiuk.ISI@gmail.com>
2017-08-07 09:24:39 -0300

    Bucle para poder probar mejor las funciones, y poder completar la palabra.

--- Principal.py
+++ Principal.py
@@ -6,41 +6,52 @@ import ImprimirEspaciado
 import MeterLetra
 import Control_LetraEnPalabra
 
+
+
+
+
 incognitaSolucion = DarIncongnita.DarIncognita()
 incognitaGuion = ""
-
-
-
-
+incognitaGuion = MeterGuiones.MeterGuiones(incognitaSolucion)
 
 print(incognitaSolucion)
 
 
-print("")
-print("")
-letra = PedirLetra.PedirLetra()
+Gano = False
+Perdio = False
 
-print("")
-print("")
-print(letra)
+while not(Gano) and not(Perdio):
 
-print("")
-print("")
+	print("")
+	print("")
+	letra = PedirLetra.PedirLetra()
 
+	print("")
+	print("")
 
-incognitaGuion = MeterGuiones.MeterGuiones(incognitaSolucion)
-
-
-ImprimirEspaciado.ImprimirEspaciado(incognitaGuion)
-
-print("")
-print("")
+	
 
 
-incognitaGuion = MeterLetra.MeterLetra(incognitaGuion,letra, Control_LetraEnPalabra.Control_LetraEnPalabra(letra,incognitaSolucion))
+	ImprimirEspaciado.ImprimirEspaciado(incognitaGuion)
 
-print("")
-print("")
-ImprimirEspaciado.ImprimirEspaciado(incognitaGuion)
+	print("")
+	print("")
 
-print(Control_LetraEnPalabra.Control_LetraEnPalabra(letra,incognitaSolucion))
+
+	incognitaGuion = MeterLetra.MeterLetra(incognitaGuion,letra, Control_LetraEnPalabra.Control_LetraEnPalabra(letra,incognitaSolucion))
+
+	print("")
+	print("")
+	ImprimirEspaciado.ImprimirEspaciado(incognitaGuion)
+
+	print(Control_LetraEnPalabra.Control_LetraEnPalabra(letra,incognitaSolucion))
+
+	if incognitaGuion.count("_")==0:
+		Gano = True
+		pass
+
+
+if Gano:
+	print("Ganaste")
+elif Perdio:
+	print("Perdiste")
