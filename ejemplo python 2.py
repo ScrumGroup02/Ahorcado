23867e545e9c758291300fd631cf701a7c44280c
nelson-nunez <30480615+nelson-nunez@users.noreply.github.com>
2017-08-07 21:07:29 -0300

    Add files via upload
    dibuja el ahorcado en base a la variable "Intentos"
    dibuja el ahorcado en base a la variable "Intentos"
    dibuja el ahorcado en base a la variable "Intentos"

---
+++ ejemplo python 2.py
@@ -0,0 +1,63 @@
+# open(nombre,modo)
+#----------------------------------------------------
+import os
+import ipdb
+#recordar hacer en la consola de CMD "pip install ipdb"
+#----------------------------------
+def hangman(Intentos)
+	
+	if Intentos = 5:
+		print("______ ")
+		print("|  ")
+		print("|  ")
+		print("|  ")
+		print("| ")
+		print("|")
+		print("|") 
+		print("____________________'esto.es.un.piso'")
+	elif Intentos = 4
+		print("______ ")
+		print("|  |")
+		print("|  ")
+		print("|  ")
+		print("| ")
+		print("|")
+		print("|") 
+		print("____________________'esto.es.un.piso'")
+	elif Intentos = 3
+		print("______ ")
+		print("|  |")
+		print("|  0")
+		print("|  ")
+		print("| ")
+		print("|")
+		print("|") 
+		print("____________________'esto.es.un.piso'")
+	elif Intentos = 2
+		print("______ ")
+		print("|  |")
+		print("|  0")
+		print("|  | ")
+		print("| ")
+		print("|")
+		print("|") 
+		print("____________________'esto.es.un.piso'")
+	elif Intentos = 1
+		print("______ ")
+		print("|  |")
+		print("|  0")
+		print("| /|\ ")
+		print("| ")
+		print("|")
+		print("|") 
+		print("____________________'esto.es.un.piso'")
+	elif Intentos = 0
+		print("______ ")
+		print("|  |")
+		print("|  0")
+		print("| /|\ ")
+		print("| / \     YOU DIE!!!")
+		print("|")
+		print("|") 
+		print("____________________'esto.es.un.piso'")
+input(" ")
