918a237cf835f9e23325fdfbb0ea97ef86e71e17
BrhmDnsK263 <Denysiuk.ISI@gmail.com>
2017-08-07 21:21:39 -0300

    Se agrego la función del hangman para que se dibuje en el juego

--- Play.py
+++ Play.py
@@ -5,6 +5,7 @@ import MeterGuiones
 import ImprimirEspaciado
 import MeterLetra
 import Control_LetraEnPalabra
+import Hangman
 
 
 
@@ -31,38 +32,45 @@ def play():
 		print("")
 		ImprimirEspaciado.ImprimirEspaciado(incognitaGuion)
 		print("")
+		Hangman.hangman(Intentos)
 		print("Intentos restantes: ", Intentos)
 		print("")
-		letra = PedirLetra.PedirLetra()
-		print("")
-		print("")
-
-		
-
-
-		
-
-		print("")
-		print("")
-
-		if Control_LetraEnPalabra.Control_LetraEnPalabra(letra,incognitaSolucion):
-			
-			incognitaGuion = MeterLetra.MeterLetra(incognitaGuion,letra, Control_LetraEnPalabra.Control_LetraEnPalabra(letra,incognitaSolucion))
-		else:
-			Intentos = Intentos-1
-
-		print("")
-		print("")
-		ImprimirEspaciado.ImprimirEspaciado(incognitaGuion)
-
-		print(Control_LetraEnPalabra.Control_LetraEnPalabra(letra,incognitaSolucion))
-
 
 		if Intentos == 0:
-			Perdio= True
-		if incognitaGuion.count("_")==0:
-			Gano = True
-			pass
+				Perdio= True
+
+		if not Perdio:
+
+			letra = PedirLetra.PedirLetra()
+			print("")
+			print("")
+
+			
+
+
+			
+
+			print("")
+			print("")
+
+			if Control_LetraEnPalabra.Control_LetraEnPalabra(letra,incognitaSolucion):
+				
+				incognitaGuion = MeterLetra.MeterLetra(incognitaGuion,letra, Control_LetraEnPalabra.Control_LetraEnPalabra(letra,incognitaSolucion))
+			else:
+				Intentos = Intentos-1
+
+			print("")
+			print("")
+			ImprimirEspaciado.ImprimirEspaciado(incognitaGuion)
+
+			print(Control_LetraEnPalabra.Control_LetraEnPalabra(letra,incognitaSolucion))
+
+
+			
+			if incognitaGuion.count("_")==0:
+				Gano = True
+				pass
+
 
 
 	if Gano:
