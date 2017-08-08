082098a0f9ed95932dab41e86c08d39a08b38f06
Facundo Toledo <fede_imfeld@hotmail.com>
2017-08-08 19:24:32 +0200

    MenuPartidas

---
+++ Letra-Rendirse.py
@@ -0,0 +1,36 @@
+# Imprimir menu:
+# 1.Decir Letra
+# 2. Rendirse
+
+# si se elige decir letra, que ejecute una función DecirLetra() 
+# //por ahora esa funcion solo imprima "diciendo letra" Después se la va reemplazar por la función correspondiente.
+# Si se elige Rendirse, que ejecute otra funcion Rendirse() 
+# //por ahora esa funcion solo imprima "rindiendose" Después se la va reemplazar por la función correspondiente.
+
+
+def imprimir():
+	print("Elegir opción")
+	print("1-Decir Letra")
+	print("2-Rendirse")
+
+
+def MenuPartida():
+
+	imprimir()
+
+	
+	opcion=int(input(""))
+
+	while (opcion !=1 and opcion != 2):
+		imprimir()
+		opcion=int(input(""))
+	pass
+
+	if (opcion==1):
+		#Pedir letra 
+
+	elif (opcion==2):
+		#Rendirse
+
+
+	print("...")
