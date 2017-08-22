fe55e85c338e0ea59f3a2e6a09f13eab7ec12a42
Facundo Toledo <facu_nahu_48@hotmail.com>
2017-08-22 21:08:47 +0200

    Administracion para no repetir palabras

--- DarIncongnita.py
+++ DarIncongnita.py
@@ -1,35 +1,39 @@
 import random
 
 
-def DarIncognita():
+# def DarIncognita():
 
 
 
 
-	return "INFORMATORIO"
+# 	return "INFORMATORIO"
 	
 	
 
 
 
-def DarIncognitaDesdeArchivo():
+def DarIncognitaDesdeArchivo():#No es eficiente, utilizar metodo readlines. 
 
 
 	archivo = open('./DATA/PALABRAS/PALABRAS', 'r')
 
 	palabras = []
-
+	
 	for x in archivo:
 		palabra = ""
 		for y in x:
 			if y!='\n':
 				palabra = palabra + y
+				
 		palabras.append(palabra)
 
+	#palabras=archivo.readlines()
+
+	return palabras
 
 
-	
+def SeleccionPalabra(palabras):
 
-	return palabras[random.randint(0,len(palabras)-1)]
-
-
+	palabra=random.choice(palabras)
+	palabras.remove(palabra)
+	return palabra,palabras
